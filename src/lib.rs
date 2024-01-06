b775182a94cec40fc86f505f054cf27bcea0819b
Magicolo <magicololand@gmail.com>
2024-01-06 04:44:38 +0000

    Added At/Is implementations

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,33 @@
 #![no_std]
 
+pub trait At<const N: usize> {
+    type Item;
+    fn at(self) -> Self::Item;
+}
+
+pub trait Is<const N: usize> {
+    fn is(&self) -> bool;
+}
+
 macro_rules! or {
-    ($n: expr, $or: ident, $module: ident $(, $types: ident, $upper: ident, $lower: ident)*) => {
-        pub type $or<$($types),*> = $module::Or<$($types),*>;
+    ($count: expr, $or: ident, $module: ident $(, $index: tt, $upper: ident, $lower: ident)*) => {
+        pub type $or<$($upper),*> = $module::Or<$($upper),*>;
 
         pub mod $module {
             #[allow(unused_imports)]
             use core::{iter, hint::unreachable_unchecked, ops::{Deref, DerefMut}};
+            #[allow(unused_imports)]
+            use super::{At, Is};
 
             #[cfg_attr(feature = "serde", derive(serde::Serialize, serde::Deserialize))]
             #[derive(Clone, Copy, Debug, PartialEq, Eq, PartialOrd, Ord, Hash)]
-            pub enum Or<$($types),*> { $($upper($types)),* }
+            pub enum Or<$($upper),*> { $($upper($upper)),* }
             #[derive(Clone, Copy, Debug)]
-            pub enum Iterator<$($types),*> { $($upper($types)),* }
+            pub enum Iterator<$($upper),*> { $($upper($upper)),* }
 
-            impl<$($types),*> Or<$($types),*> {
+            impl<$($upper),*> Or<$($upper),*> {
                 #[inline]
-                pub fn into<T>(self) -> T where $($types: Into<T>),* {
+                pub fn into<T>(self) -> T where $($upper: Into<T>),* {
                     match self {
                         $(Self::$upper(item) => item.into(),)*
                         #[allow(unreachable_patterns)]
@@ -25,7 +36,7 @@ macro_rules! or {
                 }
 
                 #[inline]
-                pub const fn as_ref(&self) -> Or<$(&$types,)*> {
+                pub const fn as_ref(&self) -> Or<$(&$upper,)*> {
                     match self {
                         $(Self::$upper(item) => Or::$upper(item),)*
                         #[allow(unreachable_patterns)]
@@ -34,7 +45,7 @@ macro_rules! or {
                 }
 
                 #[inline]
-                pub fn as_mut(&mut self) -> Or<$(&mut $types,)*> {
+                pub fn as_mut(&mut self) -> Or<$(&mut $upper,)*> {
                     match self {
                         $(Self::$upper(item) => Or::$upper(item),)*
                         #[allow(unreachable_patterns)]
@@ -44,7 +55,7 @@ macro_rules! or {
                 }
 
                 #[inline]
-                pub fn as_deref(&self) -> Or<$(&$types::Target,)*> where $($types: Deref),* {
+                pub fn as_deref(&self) -> Or<$(&$upper::Target,)*> where $($upper: Deref),* {
                     match self {
                         $(Self::$upper(item) => Or::$upper(item.deref()),)*
                         #[allow(unreachable_patterns)]
@@ -53,7 +64,7 @@ macro_rules! or {
                 }
 
                 #[inline]
-                pub fn as_deref_mut(&mut self) -> Or<$(&mut $types::Target,)*> where $($types: DerefMut),* {
+                pub fn as_deref_mut(&mut self) -> Or<$(&mut $upper::Target,)*> where $($upper: DerefMut),* {
                     match self {
                         $(Self::$upper(item) => Or::$upper(item.deref_mut()),)*
                         #[allow(unreachable_patterns)]
@@ -61,120 +72,154 @@ macro_rules! or {
 
                     }
                 }
-
-                $(
-                    #[inline]
-                    pub fn $lower(self) -> Option<$types> {
-                        #[allow(irrefutable_let_patterns)]
-                        if let Self::$upper(item) = self {
-                            Some(item)
-                        } else {
-                            None
-                        }
-                    }
-                )*
             }
 
-            impl<$($types: IntoIterator),*> IntoIterator for Or<$($types),*> {
-                type IntoIter = Iterator<$($types::IntoIter,)*>;
-                type Item = Or<$($types::Item,)*>;
+            impl<$($upper: IntoIterator),*> IntoIterator for Or<$($upper),*> {
+                type IntoIter = Iterator<$($upper::IntoIter,)*>;
+                type Item = Or<$($upper::Item,)*>;
 
                 #[inline]
                 fn into_iter(self) -> Self::IntoIter {
                     match self {
-                        $(Self::$types(item) => Iterator::$types(item.into_iter()),)*
+                        $(Self::$upper(item) => Iterator::$upper(item.into_iter()),)*
                         #[allow(unreachable_patterns)]
                         _ => unsafe { unreachable_unchecked() }
                     }
                 }
             }
 
-            impl<$($types: iter::Iterator),*> iter::Iterator for Iterator<$($types),*> {
-                type Item = Or<$($types::Item,)*>;
+            impl<$($upper: iter::Iterator),*> iter::Iterator for Iterator<$($upper),*> {
+                type Item = Or<$($upper::Item,)*>;
 
                 #[inline]
                 fn next(&mut self) -> Option<Self::Item> {
                     match self {
-                        $(Self::$types(item) => Some(Or::$types(item.next()?)),)*
+                        $(Self::$upper(item) => Some(Or::$upper(item.next()?)),)*
                         #[allow(unreachable_patterns)]
                         _ => unsafe { unreachable_unchecked() }
                     }
                 }
             }
 
-            impl<$($types: iter::DoubleEndedIterator),*> iter::DoubleEndedIterator for Iterator<$($types),*> {
+            impl<$($upper: iter::DoubleEndedIterator),*> iter::DoubleEndedIterator for Iterator<$($upper),*> {
                 #[inline]
                 fn next_back(&mut self) -> Option<Self::Item> {
                     match self {
-                        $(Self::$types(item) => Some(Or::$types(item.next_back()?)),)*
+                        $(Self::$upper(item) => Some(Or::$upper(item.next_back()?)),)*
                         #[allow(unreachable_patterns)]
                         _ => unsafe { unreachable_unchecked() }
                     }
                 }
             }
 
-            impl<$($types: iter::ExactSizeIterator),*> iter::ExactSizeIterator for Iterator<$($types),*> {
+            impl<$($upper: iter::ExactSizeIterator),*> iter::ExactSizeIterator for Iterator<$($upper),*> {
                 #[inline]
                 fn len(&self) -> usize {
                     match self {
-                        $(Self::$types(item) => item.len(),)*
+                        $(Self::$upper(item) => item.len(),)*
                         #[allow(unreachable_patterns)]
                         _ => unsafe { unreachable_unchecked() }
                     }
                 }
             }
 
-            impl<$($types: iter::FusedIterator),*> iter::FusedIterator for Iterator<$($types),*> { }
+            impl<$($upper: iter::FusedIterator),*> iter::FusedIterator for Iterator<$($upper),*> { }
+
+            or!(@outer $($upper, $index, $lower),* @ ($($upper),*));
+        }
+    };
+    (@outer $($upper: ident, $index: tt, $lower: ident),* @ $uppers: tt) => {
+        $(or!(@inner $index, $upper, $lower $uppers);)*
+    };
+    (@inner $index: tt, $upper: ident, $lower: ident ($($uppers: ident),*)) => {
+        impl<$($uppers),*> Or<$($uppers,)*> {
+            #[inline]
+            pub fn $lower(self) -> Option<$upper> {
+                #[allow(irrefutable_let_patterns)]
+                if let Self::$upper(item) = self {
+                    Some(item)
+                } else {
+                    None
+                }
+            }
+        }
+
+        impl<$($uppers),*> At<$index> for ($($uppers,)*) {
+            type Item = $upper;
+            #[inline]
+            fn at(self) -> Self::Item {
+                self.$index
+            }
+        }
+
+        impl<$($uppers),*> At<$index> for Or<$($uppers,)*> {
+            type Item = Option<$upper>;
+            #[inline]
+            fn at(self) -> Self::Item {
+                match self {
+                    Self::$upper($lower) => Some($lower),
+                    #[allow(unreachable_patterns)]
+                    _ => None,
+                }
+            }
+        }
+
+        impl<$($uppers),*> Is<$index> for Or<$($uppers,)*> {
+            #[inline]
+            fn is(&self) -> bool {
+                match self {
+                    Self::$upper(_) => true,
+                    #[allow(unreachable_patterns)]
+                    _ => false,
+                }
+            }
         }
     };
 }
 
 or!(0, Or0, or0);
-or!(1, Or1, or1, T0, T0, t0);
-or!(2, Or2, or2, T0, T0, t0, T1, T1, t1);
-or!(3, Or3, or3, T0, T0, t0, T1, T1, t1, T2, T2, t2);
-or!(4, Or4, or4, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3);
-or!(5, Or5, or5, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4);
-or!(6, Or6, or6, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5);
+or!(1, Or1, or1, 0, T0, t0);
+or!(2, Or2, or2, 0, T0, t0, 1, T1, t1);
+or!(3, Or3, or3, 0, T0, t0, 1, T1, t1, 2, T2, t2);
+or!(4, Or4, or4, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3);
+or!(5, Or5, or5, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4);
+or!(6, Or6, or6, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5);
+or!(7, Or7, or7, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6);
 or!(
-    7, Or7, or7, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6, t6
+    8, Or8, or8, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7
 );
 or!(
-    8, Or8, or8, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7
+    9, Or9, or9, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8
 );
 or!(
-    9, Or9, or9, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8
+    10, Or10, or10, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9
 );
 or!(
-    10, Or10, or10, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9
+    11, Or11, or11, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10
 );
 or!(
-    11, Or11, or11, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10
+    12, Or12, or12, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10, 11, T11, t11
 );
 or!(
-    12, Or12, or12, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10, T11, T11, t11
+    13, Or13, or13, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10, 11, T11, t11, 12, T12, t12
 );
 or!(
-    13, Or13, or13, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10, T11, T11, t11, T12, T12, t12
+    14, Or14, or14, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10, 11, T11, t11, 12, T12, t12, 13, T13, t13
 );
 or!(
-    14, Or14, or14, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10, T11, T11, t11, T12, T12, t12, T13, T13,
-    t13
+    15, Or15, or15, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10, 11, T11, t11, 12, T12, t12, 13, T13, t13, 14, T14,
+    t14
 );
 or!(
-    15, Or15, or15, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10, T11, T11, t11, T12, T12, t12, T13, T13,
-    t13, T14, T14, t14
-);
-or!(
-    16, Or16, or16, T0, T0, t0, T1, T1, t1, T2, T2, t2, T3, T3, t3, T4, T4, t4, T5, T5, t5, T6, T6,
-    t6, T7, T7, t7, T8, T8, t8, T9, T9, t9, T10, T10, t10, T11, T11, t11, T12, T12, t12, T13, T13,
-    t13, T14, T14, t14, T15, T15, t15
+    16, Or16, or16, 0, T0, t0, 1, T1, t1, 2, T2, t2, 3, T3, t3, 4, T4, t4, 5, T5, t5, 6, T6, t6, 7,
+    T7, t7, 8, T8, t8, 9, T9, t9, 10, T10, t10, 11, T11, t11, 12, T12, t12, 13, T13, t13, 14, T14,
+    t14, 15, T15, t15
 );
