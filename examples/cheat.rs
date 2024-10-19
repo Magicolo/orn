25297c496d9982b3c0f4a62d44a99b17191c2ff3
Magicolo <magicololand@gmail.com>
2024-10-19 21:44:49 +0000

    fixed typos

--- examples/cheat.rs
+++ examples/cheat.rs
@@ -3,7 +3,7 @@ use orn::*;
 /// Often, the type of iterator is conditional to some input value. Typically,
 /// to unify the return type, one would need to implement a custom iterator, but
 /// here `orn` types are used instead.
-pub fn unifies_divergent_iterator(array_or_range: bool) -> impl Iterator<Item = u8> {
+pub fn unify_divergent(array_or_range: bool) -> impl Iterator<Item = u8> {
     if array_or_range {
         Or2::T0([1u8, 2u8])
     } else {
@@ -16,7 +16,7 @@ pub fn unifies_divergent_iterator(array_or_range: bool) -> impl Iterator<Item = 
 }
 
 /// Using the `tn` methods, items of an `Or` value can be retrieved.
-pub fn retrieves_a_value_dynamically(input: Or3<u8, usize, [char; 1]>) {
+pub fn retrieve_dynamically(input: Or3<u8, usize, [char; 1]>) {
     let _a: Option<u8> = input.t0();
     let _b: Option<usize> = input.t1();
     let _c: Option<[char; 1]> = input.t2();
@@ -24,7 +24,7 @@ pub fn retrieves_a_value_dynamically(input: Or3<u8, usize, [char; 1]>) {
 
 /// Using the `At<I>` trait, items of an `Or` value can be retrieved
 /// generically.
-pub fn retrieves_a_value_statically(input: Or4<char, bool, isize, u32>) {
+pub fn retrieve_statically(input: Or4<char, bool, isize, u32>) {
     let _a: Option<char> = At::<0>::at(input);
     let _b: Option<bool> = At::<1>::at(input);
     let _c: Option<isize> = At::<2>::at(input);
